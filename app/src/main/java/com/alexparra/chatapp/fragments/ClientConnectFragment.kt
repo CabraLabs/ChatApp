95b8748545ee110ce2940c4831d075776bf34b76
Alexandre <alexandre.parra@premiersoft.net>
2021-07-22 11:41:27 -0300

    Added binding view

--- app/src/main/java/com/alexparra/chatapp/fragments/ClientConnectFragment.kt
+++ app/src/main/java/com/alexparra/chatapp/fragments/ClientConnectFragment.kt
@@ -5,20 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.alexparra.chatapp.R
+import com.alexparra.chatapp.databinding.FragmentClientConnectBinding
 
 class ClientConnectFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentClientConnectBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_client_connect, container, false)
+    ): View {
+        binding = FragmentClientConnectBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initializeButtons()
+    }
+
+    private fun initializeButtons() {
+
     }
 }
