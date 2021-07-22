6de49f01f329a024ede2a80cd9618180e5ca7435
Alexandre <alexandre.parra@premiersoft.net>
2021-07-22 13:28:29 -0300

    Refactored recycler view handler to new fun

--- app/src/main/java/com/alexparra/chatapp/fragments/ChatFragment.kt
+++ app/src/main/java/com/alexparra/chatapp/fragments/ChatFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,6 +16,8 @@ import com.alexparra.chatapp.utils.ChatManager
 
 
 class ChatFragment : Fragment() {
+
+    private val args: ChatFragmentArgs by navArgs()
 
     private lateinit var list: ArrayList<String>
 
@@ -29,6 +32,11 @@ class ChatFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initializeButtons()
+    }
+
+    private fun initializeButtons() {
 
         list = ChatManager.chatList
 
