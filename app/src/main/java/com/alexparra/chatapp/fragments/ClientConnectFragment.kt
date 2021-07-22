938bfab091f3240b3df061ff56ab5ed0cb6b78c0
Alexandre <alexandre.parra@premiersoft.net>
2021-07-22 13:28:55 -0300

    Handle buttons

--- app/src/main/java/com/alexparra/chatapp/fragments/ClientConnectFragment.kt
+++ app/src/main/java/com/alexparra/chatapp/fragments/ClientConnectFragment.kt
@@ -5,11 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.alexparra.chatapp.R
 import com.alexparra.chatapp.databinding.FragmentClientConnectBinding
+import com.alexparra.chatapp.models.ClientSocket
+import com.alexparra.chatapp.utils.toast
 
 class ClientConnectFragment : Fragment() {
 
     private lateinit var binding: FragmentClientConnectBinding
+
+    private val navController: NavController by lazy {
+        findNavController()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +36,26 @@ class ClientConnectFragment : Fragment() {
     }
 
     private fun initializeButtons() {
+        with(binding) {
+            joinChat.setOnClickListener {
+                when {
+                    username.text.toString() == ""  -> {
+                        toast(getString(R.string.username_missing))
+                    }
 
+                    ipAddress.text.toString() == "" -> {
+                        toast(getString(R.string.ip_missing))
+                    }
+
+                    else -> {
+                        val client = ClientSocket(username.text.toString(), ipAddress.text.toString(), 13)
+
+                        val action = ClientConnectFragmentDirections.actionClientConnectFragmentToChatFragment(client)
+
+                        navController.navigate(action)
+                    }
+                }
+            }
+        }
     }
 }
