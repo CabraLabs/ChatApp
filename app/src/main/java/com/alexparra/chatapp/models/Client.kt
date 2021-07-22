d5ed24e3afcee1c943ee62b04780f3678e04d6b5
Alexandre <alexandre.parra@premiersoft.net>
2021-07-22 13:29:18 -0300

    Added interface to handle server and client class

--- app/src/main/java/com/alexparra/chatapp/models/Client.kt
+++ app/src/main/java/com/alexparra/chatapp/models/Client.kt
@@ -1,18 +1,34 @@
 package com.alexparra.chatapp.models
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.io.Serializable
 import java.net.Socket
 
-class Client(host: String, port: Int) {
+interface Chat : Serializable {
+    fun writeToSocket(message: ByteArray)
+    fun updateSocket()
+}
 
-    val socket = Socket(host, port)
-    val output = socket.getOutputStream()
-    val input = socket.getInputStream()
+class ClientSocket(val username: String, ip: String, port: Int) : Chat, CoroutineScope {
+    private val parentJob = Job()
+    override val coroutineContext = parentJob + Dispatchers.IO
 
-    fun writeToSocket(message: ByteArray) {
-        output.write(message)
+    private val socket = Socket(ip, port)
+
+    override fun writeToSocket(message: ByteArray) {
+        val output = socket.getOutputStream()
+        launch {
+            output.write(message)
+        }
     }
 
-    fun updateSocket() {
-        input.read()
+    override fun updateSocket() {
+        val input = socket.getInputStream()
+        launch {
+            input.read()
+        }
     }
 }
