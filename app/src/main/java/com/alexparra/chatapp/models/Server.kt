a5403314f8a0ab8afd3623a7a84e2f38f6525002
Alexandre <alexandre.parra@premiersoft.net>
2021-07-22 14:39:56 -0300

    Added server class

--- app/src/main/java/com/alexparra/chatapp/models/Server.kt
+++ app/src/main/java/com/alexparra/chatapp/models/Server.kt
@@ -1,4 +1,23 @@
 package com.alexparra.chatapp.models
 
-class Server {
+import kotlinx.coroutines.launch
+import java.net.ServerSocket
+
+class Server : Chat {
+
+    private val serverSocket = ServerSocket(13).accept()
+
+    override fun writeToSocket(message: ByteArray) {
+        val output = serverSocket.getOutputStream()
+        output.write(message)
+    }
+
+    override fun updateSocket() {
+        val input = serverSocket.getInputStream()
+        input.read()
+    }
+
+    override fun closeSocket() {
+        serverSocket.close()
+    }
 }
