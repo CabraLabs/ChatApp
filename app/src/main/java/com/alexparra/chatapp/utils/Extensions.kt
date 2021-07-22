deb2130876b55b74be4f3d73c47bf8bfa87ba5b2
Alexandre <alexandre.parra@premiersoft.net>
2021-07-22 13:29:45 -0300

    Added toast extensions

--- app/src/main/java/com/alexparra/chatapp/utils/Extensions.kt
+++ app/src/main/java/com/alexparra/chatapp/utils/Extensions.kt
@@ -1,4 +1,12 @@
 package com.alexparra.chatapp.utils
 
-object Extensions {
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+
+fun Fragment.toast(text: String, duration: Int = Toast.LENGTH_LONG) {
+    context?.let {
+        Toast.makeText(it, text, duration).show()
+    }
 }
+
