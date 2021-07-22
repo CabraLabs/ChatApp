363ced1de8874d7d9ee0c22b79fb4b9e7dd8bc52
Alexandre <alexandre.parra@premiersoft.net>
2021-07-22 14:39:42 -0300

    Removed coroutinescope

--- app/src/main/java/com/alexparra/chatapp/models/Client.kt
+++ app/src/main/java/com/alexparra/chatapp/models/Client.kt
@@ -3,32 +3,24 @@ package com.alexparra.chatapp.models
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import java.io.Serializable
+import java.net.InetAddress
 import java.net.Socket
 
-interface Chat : Serializable {
-    fun writeToSocket(message: ByteArray)
-    fun updateSocket()
-}
-
-class ClientSocket(val username: String, ip: String, port: Int) : Chat, CoroutineScope {
-    private val parentJob = Job()
-    override val coroutineContext = parentJob + Dispatchers.IO
+class ClientSocket(val username: String, ip: InetAddress, port: Int) : Chat {
 
     private val socket = Socket(ip, port)
 
     override fun writeToSocket(message: ByteArray) {
         val output = socket.getOutputStream()
-        launch {
-            output.write(message)
-        }
+        output.write(message)
     }
 
     override fun updateSocket() {
         val input = socket.getInputStream()
-        launch {
-            input.read()
-        }
+        input.read()
+    }
+
+    override fun closeSocket() {
+        socket.close()
     }
 }
