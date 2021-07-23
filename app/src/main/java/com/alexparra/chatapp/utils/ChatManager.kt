ceb9c20c574e12e4cd7f52b8bff3fb2ae487d29f
Alexandre <alexandre.parra@premiersoft.net>
2021-07-23 14:37:34 -0300

    Started chat fragment refactoring

--- app/src/main/java/com/alexparra/chatapp/utils/ChatManager.kt
+++ app/src/main/java/com/alexparra/chatapp/utils/ChatManager.kt
@@ -1,12 +1,34 @@
 package com.alexparra.chatapp.utils
 
+import com.alexparra.chatapp.models.Server
+import kotlinx.coroutines.*
 import java.util.*
 import kotlin.collections.ArrayList
 
-object ChatManager {
+object ChatManager : CoroutineScope {
+
+    private val parentJob = Job()
+    override val coroutineContext = parentJob + Dispatchers.Main
+
     var chatList: ArrayList<String> = ArrayList()
 
     fun currentTime(): String {
         return Calendar.getInstance().time.toString()
+    }
+
+    fun runServer(server: Server) {
+        launch(Dispatchers.IO) {
+            server.startServer()
+        }
+    }
+
+    fun updateSocket(scanner: Scanner) {
+        while (scanner.hasNextLine()) {
+            list.add("s;${"received"};${scanner.nextLine()};${ChatManager.currentTime()}")
+
+            withContext(Dispatchers.Main) {
+                chatAdapter.notifyDataSetChanged()
+            }
+        }
     }
 }
