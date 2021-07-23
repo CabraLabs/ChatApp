06ca7043f1fc24f75fde2c28b10c2a972fe786a1
Alexandre <alexandre.parra@premiersoft.net>
2021-07-23 14:00:05 -0300

    Removed the need of a updateSocket function

--- app/src/main/java/com/alexparra/chatapp/models/Chat.kt
+++ app/src/main/java/com/alexparra/chatapp/models/Chat.kt
@@ -4,6 +4,5 @@ import java.io.Serializable
 
 abstract class Chat(val username: String) : Serializable {
     abstract fun writeToSocket(message: String)
-    abstract fun updateSocket()
     abstract fun closeSocket()
 }
