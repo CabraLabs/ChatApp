5474fd197ca261810d3fc4996d5372275ac04c9e
marciusps <86128468+marciusps@users.noreply.github.com>
2021-07-26 14:42:27 -0300

    attention row item

--- app/src/main/java/com/alexparra/chatapp/fragments/ChatFragment.kt
+++ app/src/main/java/com/alexparra/chatapp/fragments/ChatFragment.kt
@@ -151,6 +151,14 @@ class ChatFragment : Fragment(), CoroutineScope {
                     when {
                         message[1] == "/vibrate" -> {
                             startVibrate()
+                            ChatManager.chatList.add(
+                                Message(
+                                    MessageType.ATTENTION,
+                                    message[0],
+                                    message[1],
+                                    message[2]
+                                )
+                            )
                         }
 
                         message[3].isNotBlank() -> {
@@ -163,7 +171,7 @@ class ChatFragment : Fragment(), CoroutineScope {
                                 )
                             )
                         }
-                        
+
                         else -> {
                             ChatManager.chatList.add(
                                 Message(
