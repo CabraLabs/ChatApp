2c92b034e41b9e1e45ef23e6e8a1ab44da73a2e1
Alexandre <alexandre.parra@premiersoft.net>
2021-07-26 10:38:48 -0300

    Added message handlers that lived inside ChatFragment

--- app/src/main/java/com/alexparra/chatapp/utils/ChatManager.kt
+++ app/src/main/java/com/alexparra/chatapp/utils/ChatManager.kt
@@ -1,7 +1,14 @@
 package com.alexparra.chatapp.utils
 
+import android.content.Context
+import com.alexparra.chatapp.R
+import com.alexparra.chatapp.models.Chat
+import com.alexparra.chatapp.models.ClientSocket
 import com.alexparra.chatapp.models.Message
-import kotlinx.coroutines.*
+import com.alexparra.chatapp.models.MessageType
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -14,5 +21,29 @@ object ChatManager : CoroutineScope {
 
     fun currentTime(): String {
         return Calendar.getInstance().time.toString()
+    }
+
+    /**
+     * Sends the Message data class when the user joins the chat.
+     */
+    fun connectMessage(chat: Chat, context: Context): Message {
+        if (chat is ClientSocket) {
+            return Message(MessageType.JOINED, chat.username, context.getString(R.string.joined_the_room), currentTime())
+        }
+        return Message(MessageType.JOINED, chat.username, context.getString(R.string.created_the_room), currentTime())
+    }
+
+    /**
+     * Send the correct formatted message output to the Socket.
+     */
+    fun sendMessageToSocket(chat: Chat, text: String): String {
+        return "${chat.username};${text};${currentTime()}\n"
+    }
+
+    /**
+     * Generates the Message data class for the sent message.
+     */
+    fun getSentMessage(chat: Chat, text: String): Message {
+        return Message(MessageType.SENT, chat.username, text, currentTime())
     }
 }
