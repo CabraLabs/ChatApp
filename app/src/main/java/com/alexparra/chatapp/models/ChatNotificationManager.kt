1269eade08f1bd70cfef2857d981c061b8bb0f4c
Alexandre <alexandre.parra@premiersoft.net>
2021-07-27 13:15:17 -0300

    Implemented class

--- app/src/main/java/com/alexparra/chatapp/models/ChatNotificationManager.kt
+++ app/src/main/java/com/alexparra/chatapp/models/ChatNotificationManager.kt
@@ -1,4 +1,40 @@
 package com.alexparra.chatapp.models
 
-class ChatNotificationManager {
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import com.alexparra.chatapp.R
+
+@RequiresApi(Build.VERSION_CODES.O)
+class ChatNotificationManager(val context: Context, val channel: String) {
+
+    private val notificationChannel: NotificationChannel = NotificationChannel(channel, context.getString(R.string.app_name), NotificationManager.IMPORTANCE_DEFAULT)
+
+    private val notificationManager: NotificationManager =
+        ContextCompat.getSystemService(context, NotificationManager::class.java) as NotificationManager
+
+    fun sendMessage(username: String, text: String) {
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        val builder = NotificationCompat.Builder(
+            context,
+            channel
+        ).apply {
+            setSmallIcon(R.drawable.ic_text_message)
+            setContentTitle(username)
+            setContentText(text)
+            priority = NotificationCompat.PRIORITY_HIGH
+            color = context.getColor(R.color.blue)
+        }
+
+
+        with(NotificationManagerCompat.from(context)) {
+            notify(0, builder.build())
+        }
+    }
 }
