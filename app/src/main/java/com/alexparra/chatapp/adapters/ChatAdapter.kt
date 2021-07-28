1624ef6a0101f0e4d8f62229744f9f6d72247816
marciusps <86128468+marciusps@users.noreply.github.com>
2021-07-28 11:03:39 -0300

    chat adapters change

--- app/src/main/java/com/alexparra/chatapp/adapters/ChatAdapter.kt
+++ app/src/main/java/com/alexparra/chatapp/adapters/ChatAdapter.kt
@@ -47,38 +47,61 @@ class ChatAdapter(private val dataSet: ArrayList<Message>) :
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
 
         with(viewHolder) {
+
+            textViewUsername.text = dataSet[position].username
+            textViewMessage.text = dataSet[position].message
+            textViewDate.text = dataSet[position].time
+
             when (dataSet[position].type) {
                 MessageType.SENT -> {
                     rowLayout.gravity = Gravity.START
                     messageBackground.gravity = Gravity.START
+                    messageBackground.orientation = LinearLayout.VERTICAL
                     messageBackground.setBackgroundResource(R.drawable.send_chat_bg)
+                    angryFace.visibility = View.GONE
+                    textViewMessage.visibility = View.VISIBLE
+                    textViewDate.visibility = View.VISIBLE
+                    textViewUsername.setTypeface(null, Typeface.NORMAL)
+                    textViewMessage.setTypeface(null, Typeface.NORMAL)
                 }
 
                 MessageType.RECEIVED -> {
                     rowLayout.gravity = Gravity.END
                     messageBackground.gravity = Gravity.END
+                    messageBackground.orientation = LinearLayout.VERTICAL
                     messageBackground.setBackgroundResource(R.drawable.receive_chat_bg)
+                    angryFace.visibility = View.GONE
+                    textViewMessage.visibility = View.VISIBLE
+                    textViewDate.visibility = View.VISIBLE
+                    textViewUsername.setTypeface(null, Typeface.NORMAL)
+                    textViewMessage.setTypeface(null, Typeface.NORMAL)
                 }
 
                 MessageType.JOINED -> {
                     rowLayout.gravity = Gravity.CENTER_HORIZONTAL
+                    messageBackground.gravity = Gravity.CENTER_HORIZONTAL
                     messageBackground.orientation = LinearLayout.HORIZONTAL
+                    messageBackground.setBackgroundResource(R.drawable.event_chat_bg)
+                    angryFace.visibility = View.GONE
+                    textViewMessage.visibility = View.VISIBLE
+                    textViewDate.visibility = View.VISIBLE
                     textViewUsername.setTypeface(null, Typeface.ITALIC)
                     textViewMessage.setTypeface(null, Typeface.ITALIC)
                 }
 
                 MessageType.ATTENTION -> {
                     rowLayout.gravity = Gravity.CENTER_HORIZONTAL
+                    messageBackground.gravity = Gravity.CENTER_HORIZONTAL
+                    messageBackground.orientation = LinearLayout.HORIZONTAL
+                    messageBackground.setBackgroundResource(R.drawable.event_chat_bg)
                     angryFace.visibility = View.VISIBLE
                     textViewMessage.visibility = View.GONE
                     textViewDate.visibility = View.GONE
+                    textViewUsername.setTypeface(null, Typeface.NORMAL)
+                    textViewMessage.setTypeface(null, Typeface.NORMAL)
                 }
             }
         }
-
-        viewHolder.textViewUsername.text = dataSet[position].username
-        viewHolder.textViewMessage.text = dataSet[position].message
-        viewHolder.textViewDate.text = dataSet[position].time
     }
 
     override fun getItemCount() = dataSet.size
