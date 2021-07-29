75cb8ca546f4f3426d1bc4113294e2b4ed6f7e3a
Alexandre <alexandre.parra@premiersoft.net>
2021-07-29 12:05:35 -0300

    update

--- app/src/main/java/com/alexparra/chatapp/fragments/ClientConnectFragment.kt
+++ app/src/main/java/com/alexparra/chatapp/fragments/ClientConnectFragment.kt
@@ -1,9 +1,7 @@
 package com.alexparra.chatapp.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
