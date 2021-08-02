4c98197cf69abcfe7c67088af3eff2754239881c
Alexandre <alexandre.parra@premiersoft.net>
2021-08-02 15:28:07 -0300

    Removed intent apply
    No longer needed since single task is already on manifest

--- app/src/main/java/com/alexparra/chatapp/models/ChatNotificationManager.kt
+++ app/src/main/java/com/alexparra/chatapp/models/ChatNotificationManager.kt
@@ -28,13 +28,7 @@ class ChatNotificationManager(val context: Context, val channel: String) {
     fun sendMessage(username: String, text: String, activity: Activity) {
         notificationManager.createNotificationChannel(notificationChannel)
 
-        val intent = Intent(activity, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
-        }
-
-        val stackBuilder: TaskStackBuilder = TaskStackBuilder.create(activity)
-        stackBuilder.addParentStack(MainActivity::class.java)
-        stackBuilder.addNextIntent(intent)
+        val intent = Intent(activity, MainActivity::class.java)
 
         val pendingIntent: PendingIntent = PendingIntent.getActivity(activity, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
