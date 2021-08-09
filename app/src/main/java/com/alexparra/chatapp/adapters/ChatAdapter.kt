07858c84523d1cb489dee97a0b783e7402aa043f
marciusps <86128468+marciusps@users.noreply.github.com>
2021-08-09 16:41:39 -0300

    feat: chat adapters adjust, new chat row for vibrate, chat row changes and goat sound

--- app/src/main/java/com/alexparra/chatapp/adapters/ChatAdapter.kt
+++ app/src/main/java/com/alexparra/chatapp/adapters/ChatAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+<<<<<<< Updated upstream:app/src/main/java/com/alexparra/chatapp/adapters/ChatAdapter.kt
 import com.alexparra.chatapp.R
 import com.alexparra.chatapp.databinding.ChatRowItemEventBinding
 import com.alexparra.chatapp.databinding.ChatRowItemReceiveBinding
@@ -11,6 +12,12 @@ import com.alexparra.chatapp.databinding.ChatRowItemSendBinding
 import com.alexparra.chatapp.databinding.ChatRowItemVibrateBinding
 import com.alexparra.chatapp.models.Message
 import com.alexparra.chatapp.models.MessageType
+=======
+import com.psandroidlabs.chatapp.databinding.*
+import com.psandroidlabs.chatapp.models.Message
+import com.psandroidlabs.chatapp.models.MessageStatus
+import com.psandroidlabs.chatapp.models.MessageType
+>>>>>>> Stashed changes:app/src/main/java/com/psandroidlabs/chatapp/adapters/ChatAdapter.kt
 
 class ChatAdapter(private val dataSet: ArrayList<Message>) :
     RecyclerView.Adapter<ChatAdapter.ViewHolder>() {
@@ -19,7 +26,7 @@ class ChatAdapter(private val dataSet: ArrayList<Message>) :
         abstract fun bind(message: Message)
     }
 
-    inner class ViewHolderSendMessage(private val binding: ChatRowItemSendBinding): ViewHolder(binding.root){
+    inner class ViewHolderSendMessage(private val binding: ChatRowMessageSentBinding): ViewHolder(binding.root){
         override fun bind(message: Message) {
             with(binding){
                 chatRowUsername.text = message.username
@@ -29,7 +36,7 @@ class ChatAdapter(private val dataSet: ArrayList<Message>) :
         }
     }
 
-    inner class ViewHolderReceiveMessage(private val binding: ChatRowItemReceiveBinding): ViewHolder(binding.root){
+    inner class ViewHolderReceiveMessage(private val binding: ChatRowMessageReceivedBinding): ViewHolder(binding.root){
         override fun bind(message: Message) {
             with(binding){
                 chatRowUsername.text = message.username
@@ -39,7 +46,7 @@ class ChatAdapter(private val dataSet: ArrayList<Message>) :
         }
     }
 
-    inner class ViewHolderEventMessage(private val binding: ChatRowItemEventBinding): ViewHolder(binding.root){
+    inner class ViewHolderEventMessage(private val binding: ChatRowJoinBinding): ViewHolder(binding.root){
         override fun bind(message: Message) {
             with(binding){
                 chatRowUsername.text = message.username
@@ -49,37 +56,83 @@ class ChatAdapter(private val dataSet: ArrayList<Message>) :
         }
     }
 
+<<<<<<< Updated upstream:app/src/main/java/com/alexparra/chatapp/adapters/ChatAdapter.kt
     inner class ViewHolderVibrateMessage(private val binding: ChatRowItemVibrateBinding): ViewHolder(binding.root){
         override fun bind(message: Message) {
             with(binding){
                 chatRowUsername.text = message.username
                 chatRowImage.setImageResource(R.drawable.ic_vibrate)
+=======
+    inner class ViewHolderReceivedVibrate(private val binding: ChatRowVibrateReceivedBinding): ViewHolder(binding.root){
+        override fun bind(message: Message) {
+            with(binding){
+                chatRowUsername.text = message.username
+                chatRowTime.text = message.time
+            }
+        }
+    }
+
+    inner class ViewHolderSentVibrate(private val binding: ChatRowVibrateSentBinding): ViewHolder(binding.root){
+        override fun bind(message: Message) {
+            with(binding){
+                chatRowUsername.text = message.username
+>>>>>>> Stashed changes:app/src/main/java/com/psandroidlabs/chatapp/adapters/ChatAdapter.kt
                 chatRowTime.text = message.time
             }
         }
     }
 
     override fun getItemViewType(position: Int): Int {
+<<<<<<< Updated upstream:app/src/main/java/com/alexparra/chatapp/adapters/ChatAdapter.kt
         return when (dataSet[position].type){
             MessageType.SENT -> 1
             MessageType.RECEIVED -> 2
             MessageType.JOINED -> 3
             else -> 3
+=======
+        return viewTypeTranslator(dataSet[position].type, dataSet[position].status)
+    }
+
+    private fun viewTypeTranslator(type: MessageType, status: MessageStatus): Int {
+        var number = 0
+        if (status != MessageStatus.SENT) number = 7
+        return when (type) {
+            MessageType.MESSAGE -> MessageType.MESSAGE.code + number
+            MessageType.JOIN -> MessageType.JOIN.code + number
+            MessageType.VIBRATE -> MessageType.VIBRATE.code + number
+            MessageType.AUDIO -> MessageType.AUDIO.code + number
+            MessageType.IMAGE -> MessageType.IMAGE.code + number
+            MessageType.TICINVITE -> MessageType.TICINVITE.code + number
+            MessageType.TICPLAY -> MessageType.TICPLAY.code + number
+>>>>>>> Stashed changes:app/src/main/java/com/psandroidlabs/chatapp/adapters/ChatAdapter.kt
         }
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         return when (viewType){
-            1 -> {
-                ViewHolderSendMessage(ChatRowItemSendBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false))
+            0 -> {
+                ViewHolderSendMessage(ChatRowMessageSentBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false))
             }
 
             2 -> {
-                ViewHolderReceiveMessage(ChatRowItemReceiveBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false))
+                ViewHolderSentVibrate(ChatRowVibrateSentBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false))
             }
 
+            7 -> {
+                ViewHolderReceiveMessage(ChatRowMessageReceivedBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false))
+            }
+
+            9 -> {
+                ViewHolderReceivedVibrate(ChatRowVibrateReceivedBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false))
+            }
+
+<<<<<<< Updated upstream:app/src/main/java/com/alexparra/chatapp/adapters/ChatAdapter.kt
             3 -> {
                 ViewHolderEventMessage(ChatRowItemEventBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false))
+=======
+            else -> {
+                ViewHolderEventMessage(ChatRowJoinBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false))
+>>>>>>> Stashed changes:app/src/main/java/com/psandroidlabs/chatapp/adapters/ChatAdapter.kt
             }
 
             else -> {
@@ -89,7 +142,6 @@ class ChatAdapter(private val dataSet: ArrayList<Message>) :
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-
         val message = dataSet[position]
         viewHolder.bind(message)
     }
