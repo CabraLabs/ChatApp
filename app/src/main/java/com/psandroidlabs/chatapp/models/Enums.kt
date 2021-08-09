b029a17631cc0d59f77c5bca6b30c0a4fa0c67bb
Alexandre <alexandre.parra@premiersoft.net>
2021-08-09 11:32:59 -0300

    Refactored and new enum

--- app/src/main/java/com/psandroidlabs/chatapp/models/Enums.kt
+++ app/src/main/java/com/psandroidlabs/chatapp/models/Enums.kt
@@ -3,3 +3,11 @@ package com.psandroidlabs.chatapp.models
 enum class UserType {
     CLIENT, SERVER
 }
+
+enum class MessageType(val code: Int) {
+    MESSAGE(0), JOIN(1), VIBRATE(2), AUDIO(3), IMAGE(4), TICINVITE(5), TICPLAY(6)
+}
+
+enum class MessageStatus {
+    RECEIVED, SENT
+}
