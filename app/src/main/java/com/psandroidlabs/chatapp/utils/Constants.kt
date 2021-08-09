02d1af07fa243a9b8bcac003a13b1b832de175e9
Alexandre <alexandre.parra@premiersoft.net>
2021-08-09 16:41:37 -0300

    Added new constants
    New constants for shared preferences, message commands and blank lines have been added between the different categories of constants for ease of use.

--- app/src/main/java/com/psandroidlabs/chatapp/utils/Constants.kt
+++ app/src/main/java/com/psandroidlabs/chatapp/utils/Constants.kt
@@ -2,5 +2,11 @@ package com.psandroidlabs.chatapp.utils
 
 object Constants {
     const val ACTION_STOP = "STOP"
+
     const val PRIMARY_CHAT_CHANNEL = "0"
+
+    const val VIBRATE_COMMAND = "/vibrate"
+
+    const val CLIENT_USER = "client_user"
+    const val CLIENT_IP = "client_ip"
 }
