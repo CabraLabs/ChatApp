5883503087fcb666d967142b2abee6099ba1fe66
Alexandre <alexandre.parra@premiersoft.net>
2021-08-10 10:55:20 -0300

    Added port and channel constant

--- app/src/main/java/com/psandroidlabs/chatapp/utils/Constants.kt
+++ app/src/main/java/com/psandroidlabs/chatapp/utils/Constants.kt
@@ -1,9 +1,12 @@
 package com.psandroidlabs.chatapp.utils
 
 object Constants {
+    const val CHAT_DEFAULT_PORT = 1027
+
     const val ACTION_STOP = "STOP"
 
     const val PRIMARY_CHAT_CHANNEL = "0"
+    const val FOREGROUND_CHAT_CHANNEL = "foreground"
 
     const val VIBRATE_COMMAND = "/vibrate"
 
