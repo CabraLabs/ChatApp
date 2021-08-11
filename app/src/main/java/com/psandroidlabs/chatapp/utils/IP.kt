c44f437f91a3a79d251141779f5121042e816b7c
Alexandre <alexandre.parra@premiersoft.net>
2021-08-11 15:47:18 -0300

    Moved IP function here

--- app/src/main/java/com/psandroidlabs/chatapp/utils/IP.kt
+++ app/src/main/java/com/psandroidlabs/chatapp/utils/IP.kt
@@ -1,4 +1,29 @@
 package com.psandroidlabs.chatapp.utils
 
-object Ip {
+import kotlinx.coroutines.*
+import java.net.DatagramSocket
+import java.net.InetAddress
+
+object IP : CoroutineScope {
+    private val parentJob = Job()
+    override val coroutineContext = parentJob + Dispatchers.IO
+
+    /**
+     * Use a datagram socket to find the device IP on the local network.
+     */
+    fun getIpAddress(): String {
+        var ip = ""
+
+        runBlocking {
+            launch(Dispatchers.IO) {
+                DatagramSocket().use { socket ->
+                    socket.connect(InetAddress.getByName("8.8.8.8"), Constants.CHAT_DEFAULT_PORT)
+                    ip = socket.localAddress.hostAddress
+                    socket.close()
+                }
+            }
+        }
+
+        return ip
+    }
 }
