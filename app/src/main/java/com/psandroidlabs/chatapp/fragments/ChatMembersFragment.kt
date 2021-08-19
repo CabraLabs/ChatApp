d5329b78b7771353e9f5df12ffdbe396cae76ce4
marciusps <86128468+marciusps@users.noreply.github.com>
2021-08-19 12:02:39 -0300

    added chat members class

--- app/src/main/java/com/psandroidlabs/chatapp/fragments/ChatMembersFragment.kt
+++ app/src/main/java/com/psandroidlabs/chatapp/fragments/ChatMembersFragment.kt
@@ -1,4 +1,47 @@
 package com.psandroidlabs.chatapp.fragments
 
-class ChatMembersFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.psandroidlabs.chatapp.adapters.ChatMembersAdapter
+import com.psandroidlabs.chatapp.databinding.FragmentChatMembersBinding
+import com.psandroidlabs.chatapp.utils.ChatManager
+
+class ChatMembersFragment: Fragment() {
+
+    private lateinit var binding: FragmentChatMembersBinding
+    private lateinit var chatMembersAdapter: ChatMembersAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentChatMembersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initializeList()
+    }
+
+    private fun initializeList(){
+        val recyclerViewList: RecyclerView = binding.tableRecycler
+        chatMembersAdapter = ChatMembersAdapter(ChatManager.chatMembersList, ::onCellClick)
+
+        recyclerViewList.apply {
+            adapter = chatMembersAdapter
+            layoutManager = GridLayoutManager(context, 3)
+        }
+    }
+
+    private fun onCellClick(pos: Int) {
+        //TODO send tictactoe invite
+    }
 }
