f2c528ce6112f6edb5c3dbf9449b2cc9670e2e8f
Alexandre <alexandre.parra@premiersoft.net>
2021-08-20 10:05:14 -0300

    feat: check ip format with regex

--- app/src/main/java/com/psandroidlabs/chatapp/viewmodels/ClientViewModel.kt
+++ app/src/main/java/com/psandroidlabs/chatapp/viewmodels/ClientViewModel.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.*
 import java.net.InetAddress
 import java.net.Socket
 import java.util.*
+import java.util.regex.Pattern
 
 class ClientViewModel : ViewModel(), CoroutineScope {
 
@@ -138,6 +139,11 @@ class ClientViewModel : ViewModel(), CoroutineScope {
     }
 
     fun transformIp(text: String): InetAddress = InetAddress.getByName(text)
+
+    fun validateIp(text: String): Boolean {
+        val ipRegex = "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$";
+        return Pattern.matches(ipRegex, text)
+    }
 
     fun closeSocket() {
         running = false
