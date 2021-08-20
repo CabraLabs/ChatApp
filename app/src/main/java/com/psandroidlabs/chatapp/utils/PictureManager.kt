fdee755d82cbeaf4cd3ecfe57b5f74bed72ea574
marciusps <86128468+marciusps@users.noreply.github.com>
2021-08-20 15:17:39 -0300

    added defaultAvatar value and bitmapToFile function

--- app/src/main/java/com/psandroidlabs/chatapp/utils/PictureManager.kt
+++ app/src/main/java/com/psandroidlabs/chatapp/utils/PictureManager.kt
@@ -1,17 +1,33 @@
 package com.psandroidlabs.chatapp.utils
 
 import android.content.ContentResolver
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
+import android.os.Environment
 import androidx.core.graphics.drawable.toDrawable
 import java.io.ByteArrayOutputStream
+import java.io.File
 import java.util.*
+import android.provider.MediaStore.Images
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import com.psandroidlabs.chatapp.MainApplication
+import com.psandroidlabs.chatapp.R
+import java.io.FileOutputStream
+import java.io.IOException
+
 
 object PictureManager {
+
+    val defaultAvatar by lazy {
+        MainApplication.applicationContext().let { ContextCompat.getDrawable(it, R.drawable.ic_goat)?.toBitmap(200, 200) }
+    }
 
     private fun bitmapToByteArray(bitmap: Bitmap): ByteArray {
         val bmp = bitmap.toDrawable(Resources.getSystem()).bitmap
@@ -24,7 +40,7 @@ object PictureManager {
         return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
     }
 
-    fun compressBitmap(bitmap: Bitmap): Bitmap {
+    private fun compressBitmap(bitmap: Bitmap): Bitmap {
         val stream = ByteArrayOutputStream()
         bitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream)
         val byteArray = stream.toByteArray()
@@ -49,6 +65,11 @@ object PictureManager {
         }
     }
 
+    fun bitmapToUri(bitmap: Bitmap, contentResolver: ContentResolver): Uri {
+        val path = Images.Media.insertImage(contentResolver, bitmap, "avatar", null)
+        return Uri.parse(path)
+    }
+
     fun bitmapToString(bitmap: Bitmap): String {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             Base64.getEncoder().encodeToString(bitmapToByteArray(compressBitmap(bitmap)))
@@ -65,5 +86,23 @@ object PictureManager {
             Bitmap.createBitmap(50, 50, Bitmap.Config.ARGB_8888)
             //TODO
         }
+    }
+
+    fun bitmapToFile(context: Context?, bitmap: Bitmap): Uri {
+        val wrapper = ContextWrapper(MainApplication.applicationContext())
+
+        var file = wrapper.getDir("Images", Context.MODE_PRIVATE)
+        file = File(file, "${UUID.randomUUID()}.jpg")
+
+        try {
+            val stream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100, stream)
+            stream.flush()
+            stream.close()
+        } catch (e: IOException){
+            e.printStackTrace()
+        }
+
+        return Uri.parse(file.absolutePath)
     }
 }
