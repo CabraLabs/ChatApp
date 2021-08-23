0bcf4764df0984c8d44e5681d14f96a536ec55eb
marciusps <86128468+marciusps@users.noreply.github.com>
2021-08-23 11:07:51 -0300

    fixed total number in viewTypeTranslator

--- app/src/main/java/com/psandroidlabs/chatapp/adapters/ChatAdapter.kt
+++ app/src/main/java/com/psandroidlabs/chatapp/adapters/ChatAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.psandroidlabs.chatapp.R
 import com.psandroidlabs.chatapp.databinding.*
 import com.psandroidlabs.chatapp.models.Message
 import com.psandroidlabs.chatapp.models.MessageStatus
