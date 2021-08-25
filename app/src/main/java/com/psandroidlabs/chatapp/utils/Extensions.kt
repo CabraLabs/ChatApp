a20067827a2ce1f1ccbc1afbbc9f780eb14990b5
marciusps <86128468+marciusps@users.noreply.github.com>
2021-08-25 15:41:08 -0300

    added Bitmap.toSquare extension

--- app/src/main/java/com/psandroidlabs/chatapp/utils/Extensions.kt
+++ app/src/main/java/com/psandroidlabs/chatapp/utils/Extensions.kt
@@ -1,11 +1,16 @@
 package com.psandroidlabs.chatapp.utils
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.psandroidlabs.chatapp.MainApplication.Companion.applicationContext
+import java.io.IOException
 import java.security.MessageDigest
+import java.util.Collections.min
+import kotlin.math.min
 
 
 fun Fragment.toast(text: String, duration: Int = Toast.LENGTH_LONG) {
@@ -29,4 +34,19 @@ fun Fragment.hideKeyboard() {
 fun String.toSHA256(): String {
     val messageDigest = MessageDigest.getInstance("SHA-256").digest(toByteArray())
     return messageDigest.fold("", { str, it -> str + "%02x".format(it) })
+}
+
+fun Bitmap.toSquare():Bitmap?{
+    val side = min(width,height)
+
+    val xOffset = (width - side) /2
+    val yOffset = (height - side)/2
+
+    return Bitmap.createBitmap(
+        this,
+        xOffset,
+        yOffset,
+        side,
+        side
+    )
 }
