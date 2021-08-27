2baffc3af62fa6643517b7078356f4d81347e4ad
Alexandre <alexandre.parra@premiersoft.net>
2021-08-27 16:28:13 -0300

    feat: audio manager

--- app/src/main/java/com/psandroidlabs/chatapp/utils/AudioManager.kt
+++ app/src/main/java/com/psandroidlabs/chatapp/utils/AudioManager.kt
@@ -1,4 +1,33 @@
 package com.psandroidlabs.chatapp.utils
 
+import android.content.Context
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.os.Build
+import android.util.Log
+import java.io.File
+import java.io.IOException
+
 object AudioManager {
+    /**
+     * Create and return a MediaRecorder to record audio messages.
+     */
+    fun createAudioRecorder(audioPath: String) = MediaRecorder().apply {
+        setAudioSource(MediaRecorder.AudioSource.MIC)
+        setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+
+        setOutputFile(audioPath)
+    }
+
+    fun nameAudio(): String = ChatManager.getEpoch().toString() + ".mp3"
+
+
+    fun audioDir(fileName: String, context: Context): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            File(context.getExternalFilesDir(Constants.AUDIO_DIR), fileName).toString()
+        } else {
+            File(context.cacheDir, fileName).toString()
+        }
+    }
 }
