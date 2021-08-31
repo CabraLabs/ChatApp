69bfddf77e54cdef726affb93ce36617eb57a01c
Alexandre <alexandre.parra@premiersoft.net>
2021-08-31 13:12:46 -0300

    fix: removed unused imports

--- app/src/main/java/com/psandroidlabs/chatapp/fragments/ProfileFragment.kt
+++ app/src/main/java/com/psandroidlabs/chatapp/fragments/ProfileFragment.kt
@@ -1,11 +1,9 @@
 package com.psandroidlabs.chatapp.fragments
 
 import android.Manifest
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
