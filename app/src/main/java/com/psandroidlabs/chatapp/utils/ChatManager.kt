c3b536d69e640700f025c16bb0a342bd6e64228b
marciusps <86128468+marciusps@users.noreply.github.com>
2021-09-01 15:46:43 -0300

    update

--- app/src/main/java/com/psandroidlabs/chatapp/utils/ChatManager.kt
+++ app/src/main/java/com/psandroidlabs/chatapp/utils/ChatManager.kt
@@ -160,12 +160,15 @@ object ChatManager : CoroutineScope {
         id = id
     )
 
-    fun imageMessage(username: String, imagePath: String?, bitmap: Bitmap) = createMessage(
-        type = MessageType.IMAGE,
-        username = username,
-        text = imagePath ?: throw Exception("Image message needs to have a full path."),
-        base64Data = bitmap.toBase64()
-    )
+    fun imageMessage(username: String, imagePath: String?, bitmap: Bitmap): Message {
+        imagePath?.let {
+            return  createMessage(
+                type = MessageType.IMAGE,
+                username = username,
+                base64Data = bitmap.toBase64()
+            )
+        } ?: throw Exception("Audio message needs to have a full path.")
+    }
 
     /**
      * Parse the message to a valid REVOKED or ACKNOWLEDGE message.
