8330c9a976a1761641337276941b96213a555014
Alexandre <alexandre.parra@premiersoft.net>
2021-09-03 11:00:41 -0300

    feat: tictactoe server outline

--- app/src/main/java/com/psandroidlabs/chatapp/tictactoe/utils/TicTacToeServer.kt
+++ app/src/main/java/com/psandroidlabs/chatapp/tictactoe/utils/TicTacToeServer.kt
@@ -1,13 +1,30 @@
 package com.psandroidlabs.chatapp.tictactoe.utils
 
+import com.psandroidlabs.chatapp.models.Message
+import com.psandroidlabs.chatapp.models.MessageType
 import java.util.ArrayList
 import java.util.HashMap
+
 
 object TicTacToeServer {
     val ticTacToeGames: HashMap<Pair<Int, Int>, ArrayList<Int>> = hashMapOf()
 
-    fun parsePlay() {
-        TODO()
+    fun parseMessage(message: Message) {
+        val status  = when(message.type) {
+            MessageType.TIC_INVITE.code -> {
+                TODO()
+                // return another tic_invite
+            }
+
+            MessageType.TIC_PLAY.code -> {
+                checkBoard()
+                // return the result of the play, valid, not valid, win or lose.
+            }
+
+            else -> {
+                // return an error
+            }
+        }
     }
 
     private fun checkBoard() {
